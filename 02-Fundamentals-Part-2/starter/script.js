8cac3042db7ff30f57474b4a0165f2caf77e9d97
Jaden Le <ple030999@gmail.com>
2026-01-03 00:59:12 -0800

    I learned about function declaration, function expression, and arrow function. It is not hard at all, but I need time to familarize them.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 /*
-'use strict'; //activated strict mode 
+'use strict'; //activated strict mode
 //always put strict mode to avoid accidental errors.
 //it will help to show the errors in console.
 
@@ -40,3 +40,47 @@ console.log(fruitProcessor(5, 0));
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 */
+
+
+/*
+//Continue function
+//Function declaration
+const age1 = calcAge1(1991); //can access before initialized.
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+//Function expression
+const calcAge2 = function (birthYear) { //Anonymous function
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+*/
+
+
+
+/*
+//Next lesson
+//Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jaden'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+*/
+
+
+
+//Next lesson
+//Functions Calling other Functions
