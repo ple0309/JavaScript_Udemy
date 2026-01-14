6616e0577f78edeb36c7a6dbca6a5b6352418a77
Jaden Le <ple030999@gmail.com>
2026-01-13 23:47:54 -0800

    Today I learned about using classList to remove and add class of hidden. For example, when I click on the button, it will show the pop up paragraph.

---
+++ 06-Modal/starter/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+//Only the first one will be selected.
+// const btnsOpenModal = document.querySelector('.show-modal');
+
+//Therefore, we will need another one.
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+
+  // modal.style.display = 'block';
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
