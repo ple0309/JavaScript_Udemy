88522a92e79e292eddab0016fca4d3ebf0c59b52
Jaden Le <ple030999@gmail.com>
2026-01-14 23:14:16 -0800

    I just learned abou how to set up pressing Esc to escape. Setting up document.addEventListener with keydown and function(e). The argument e will be the pressed key, and e.key will return the name of that key.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -11,7 +11,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 
@@ -29,3 +28,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//This document will add function for all not part in html.
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
