1c249879ce99543169a58a2e6661554439af1e84
Jaden Le <ple030999@gmail.com>
2026-02-06 23:36:26 -0800

    Today, I learned about the destructuring arrays. It is going to be the same with copy element to variable. However, in this time, the variable will be the array, and when we assign the list of array with values, those values will be matched the position into array variable.

---
+++ 09-Data-Structures-Operators/starter/script.js
@@ -0,0 +1,98 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    //We are not destructuring yet.
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+//Normal way we will create like this
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//But other can create shorter.
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+// //To get the first and third element, there will be a hole , , in the middle.
+// const [first, , second] = restaurant.categories;
+// console.log(first, second);
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// //To swap each variable. To do that we should change const in [main, secondary] to let.
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+//In order to switch to two variables using destructuring arrays.
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//Another trick
+//Receive 2 return vales from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//Nested destructuring
+const nested = [2, 4, [5, 6]];
+
+// //Destructuring assignment;
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r); //r will be undefined if not set r = 1
