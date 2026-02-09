37e4b9edf8b0d2e8f24143feb7dc7a9b9c1a9597
Jaden Le <ple030999@gmail.com>
2026-02-08 23:51:39 -0800

    Today I learned about the spread operator ... . It is really helpful to copy from object. It will be shortcut to copy the array or the object faster that other way. And this way is in mordern ES6+

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -22,35 +22,81 @@ const mexicanFoods = new Set([
   'garlic',
 ]);
 
-// // Data needed for first part of the section
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-//   order: function (starterIndex, mainIndex) {
-//     //We are not destructuring yet.
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//   },
+  order: function (starterIndex, mainIndex) {
+    //We are not destructuring yet.
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(obj);
+  },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+/////////////////////////////////////////////////////////////////
+//////////////////////// Destructuring Objects /////////////////
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
 
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-// };
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`,
+    );
+  },
+});
 
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// //Setting default value, it should be the same with destructuring array.
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// //Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+// console.log(a, b);
+
+// //Nested objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+/////////////////////////////////////////////////////////////////
+///////////////// Destructuring Arrays /////////////////////////
 // //Normal way we will create like this
 // const arr = [2, 3, 4];
 // const a = arr[0];
@@ -98,244 +144,302 @@ const mexicanFoods = new Set([
 // console.log(p, q, r); //r will be undefined if not set r = 1
 /////////////////////////////////////////////////////////////////
 ////////////////////// Chanllenges /////////////////////////////
-const books = [
-  {
-    title: 'Algorithms',
-    author: ['Robert Sedgewick', 'Kevin Wayne'],
-    publisher: 'Addison-Wesley Professional',
-    publicationDate: '2011-03-24',
-    edition: 4,
-    keywords: [
-      'computer science',
-      'programming',
-      'algorithms',
-      'data structures',
-      'java',
-      'math',
-      'software',
-      'engineering',
-    ],
-    pages: 976,
-    format: 'hardcover',
-    ISBN: '9780321573513',
-    language: 'English',
-    programmingLanguage: 'Java',
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.41,
-        ratingsCount: 1733,
-        reviewsCount: 63,
-        fiveStarRatingCount: 976,
-        oneStarRatingCount: 13,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'Structure and Interpretation of Computer Programs',
-    author: [
-      'Harold Abelson',
-      'Gerald Jay Sussman',
-      'Julie Sussman (Contributor)',
-    ],
-    publisher: 'The MIT Press',
-    publicationDate: '2022-04-12',
-    edition: 2,
-    keywords: [
-      'computer science',
-      'programming',
-      'javascript',
-      'software',
-      'engineering',
-    ],
-    pages: 640,
-    format: 'paperback',
-    ISBN: '9780262543231',
-    language: 'English',
-    programmingLanguage: 'JavaScript',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.36,
-        ratingsCount: 14,
-        reviewsCount: 3,
-        fiveStarRatingCount: 8,
-        oneStarRatingCount: 0,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: "Computer Systems: A Programmer's Perspective",
-    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
-    publisher: 'Prentice Hall',
-    publicationDate: '2002-01-01',
-    edition: 1,
-    keywords: [
-      'computer science',
-      'computer systems',
-      'programming',
-      'software',
-      'C',
-      'engineering',
-    ],
-    pages: 978,
-    format: 'hardcover',
-    ISBN: '9780130340740',
-    language: 'English',
-    programmingLanguage: 'C',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.44,
-        ratingsCount: 1010,
-        reviewsCount: 57,
-        fiveStarRatingCount: 638,
-        oneStarRatingCount: 16,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'Operating System Concepts',
-    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
-    publisher: 'John Wiley & Sons',
-    publicationDate: '2004-12-14',
-    edition: 10,
-    keywords: [
-      'computer science',
-      'operating systems',
-      'programming',
-      'software',
-      'C',
-      'Java',
-      'engineering',
-    ],
-    pages: 921,
-    format: 'hardcover',
-    ISBN: '9780471694663',
-    language: 'English',
-    programmingLanguage: 'C, Java',
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 3.9,
-        ratingsCount: 2131,
-        reviewsCount: 114,
-        fiveStarRatingCount: 728,
-        oneStarRatingCount: 65,
-      },
-    },
-  },
-  {
-    title: 'Engineering Mathematics',
-    author: ['K.A. Stroud', 'Dexter J. Booth'],
-    publisher: 'Palgrave',
-    publicationDate: '2007-01-01',
-    edition: 14,
-    keywords: ['mathematics', 'engineering'],
-    pages: 1288,
-    format: 'paperback',
-    ISBN: '9781403942463',
-    language: 'English',
-    programmingLanguage: null,
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.35,
-        ratingsCount: 370,
-        reviewsCount: 18,
-        fiveStarRatingCount: 211,
-        oneStarRatingCount: 6,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: 'The Personal MBA: Master the Art of Business',
-    author: 'Josh Kaufman',
-    publisher: 'Portfolio',
-    publicationDate: '2010-12-30',
-    keywords: ['business'],
-    pages: 416,
-    format: 'hardcover',
-    ISBN: '9781591843528',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.11,
-        ratingsCount: 40119,
-        reviewsCount: 1351,
-        fiveStarRatingCount: 18033,
-        oneStarRatingCount: 1090,
-      },
-    },
-  },
-  {
-    title: 'Crafting Interpreters',
-    author: 'Robert Nystrom',
-    publisher: 'Genever Benning',
-    publicationDate: '2021-07-28',
-    keywords: [
-      'computer science',
-      'compilers',
-      'engineering',
-      'interpreters',
-      'software',
-      'engineering',
-    ],
-    pages: 865,
-    format: 'paperback',
-    ISBN: '9780990582939',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.7,
-        ratingsCount: 253,
-        reviewsCount: 23,
-        fiveStarRatingCount: 193,
-        oneStarRatingCount: 0,
-      },
-    },
-  },
-  {
-    title: 'Deep Work: Rules for Focused Success in a Distracted World',
-    author: 'Cal Newport',
-    publisher: 'Grand Central Publishing',
-    publicationDate: '2016-01-05',
-    edition: 1,
-    keywords: ['work', 'focus', 'personal development', 'business'],
-    pages: 296,
-    format: 'hardcover',
-    ISBN: '9781455586691',
-    language: 'English',
-    thirdParty: {
-      goodreads: {
-        rating: 4.19,
-        ratingsCount: 144584,
-        reviewsCount: 11598,
-        fiveStarRatingCount: 63405,
-        oneStarRatingCount: 1808,
-      },
-    },
-    highlighted: true,
-  },
+// const books = [
+//   {
+//     title: 'Algorithms',
+//     author: ['Robert Sedgewick', 'Kevin Wayne'],
+//     publisher: 'Addison-Wesley Professional',
+//     publicationDate: '2011-03-24',
+//     edition: 4,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'algorithms',
+//       'data structures',
+//       'java',
+//       'math',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 976,
+//     format: 'hardcover',
+//     ISBN: '9780321573513',
+//     language: 'English',
+//     programmingLanguage: 'Java',
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.41,
+//         ratingsCount: 1733,
+//         reviewsCount: 63,
+//         fiveStarRatingCount: 976,
+//         oneStarRatingCount: 13,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Structure and Interpretation of Computer Programs',
+//     author: [
+//       'Harold Abelson',
+//       'Gerald Jay Sussman',
+//       'Julie Sussman (Contributor)',
+//     ],
+//     publisher: 'The MIT Press',
+//     publicationDate: '2022-04-12',
+//     edition: 2,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'javascript',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 640,
+//     format: 'paperback',
+//     ISBN: '9780262543231',
+//     language: 'English',
+//     programmingLanguage: 'JavaScript',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.36,
+//         ratingsCount: 14,
+//         reviewsCount: 3,
+//         fiveStarRatingCount: 8,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "Computer Systems: A Programmer's Perspective",
+//     author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+//     publisher: 'Prentice Hall',
+//     publicationDate: '2002-01-01',
+//     edition: 1,
+//     keywords: [
+//       'computer science',
+//       'computer systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'engineering',
+//     ],
+//     pages: 978,
+//     format: 'hardcover',
+//     ISBN: '9780130340740',
+//     language: 'English',
+//     programmingLanguage: 'C',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.44,
+//         ratingsCount: 1010,
+//         reviewsCount: 57,
+//         fiveStarRatingCount: 638,
+//         oneStarRatingCount: 16,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Operating System Concepts',
+//     author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+//     publisher: 'John Wiley & Sons',
+//     publicationDate: '2004-12-14',
+//     edition: 10,
+//     keywords: [
+//       'computer science',
+//       'operating systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'Java',
+//       'engineering',
+//     ],
+//     pages: 921,
+//     format: 'hardcover',
+//     ISBN: '9780471694663',
+//     language: 'English',
+//     programmingLanguage: 'C, Java',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 3.9,
+//         ratingsCount: 2131,
+//         reviewsCount: 114,
+//         fiveStarRatingCount: 728,
+//         oneStarRatingCount: 65,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Engineering Mathematics',
+//     author: ['K.A. Stroud', 'Dexter J. Booth'],
+//     publisher: 'Palgrave',
+//     publicationDate: '2007-01-01',
+//     edition: 14,
+//     keywords: ['mathematics', 'engineering'],
+//     pages: 1288,
+//     format: 'paperback',
+//     ISBN: '9781403942463',
+//     language: 'English',
+//     programmingLanguage: null,
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.35,
+//         ratingsCount: 370,
+//         reviewsCount: 18,
+//         fiveStarRatingCount: 211,
+//         oneStarRatingCount: 6,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'The Personal MBA: Master the Art of Business',
+//     author: 'Josh Kaufman',
+//     publisher: 'Portfolio',
+//     publicationDate: '2010-12-30',
+//     keywords: ['business'],
+//     pages: 416,
+//     format: 'hardcover',
+//     ISBN: '9781591843528',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.11,
+//         ratingsCount: 40119,
+//         reviewsCount: 1351,
+//         fiveStarRatingCount: 18033,
+//         oneStarRatingCount: 1090,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Crafting Interpreters',
+//     author: 'Robert Nystrom',
+//     publisher: 'Genever Benning',
+//     publicationDate: '2021-07-28',
+//     keywords: [
+//       'computer science',
+//       'compilers',
+//       'engineering',
+//       'interpreters',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 865,
+//     format: 'paperback',
+//     ISBN: '9780990582939',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.7,
+//         ratingsCount: 253,
+//         reviewsCount: 23,
+//         fiveStarRatingCount: 193,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Deep Work: Rules for Focused Success in a Distracted World',
+//     author: 'Cal Newport',
+//     publisher: 'Grand Central Publishing',
+//     publicationDate: '2016-01-05',
+//     edition: 1,
+//     keywords: ['work', 'focus', 'personal development', 'business'],
+//     pages: 296,
+//     format: 'hardcover',
+//     ISBN: '9781455586691',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.19,
+//         ratingsCount: 144584,
+//         reviewsCount: 11598,
+//         fiveStarRatingCount: 63405,
+//         oneStarRatingCount: 1808,
+//       },
+//     },
+//     highlighted: true,
+//   },
+// ];
+// //1.1 destructure the books array into two variables called firstBook and secondBook.
+// const [firstBook, secondBook] = books;
+
+// //1.2 Destructure the books array into a variable called thirdBook. You must skip the first two books.
+// const [, , thirdBook] = books;
+
+// //1.3 setting the rating and ratingsCount variable with number in ratings.
+// const ratings = [
+//   ['rating', 4.19],
+//   ['ratingsCount', 144584],
+// ];
+// const [[, rating], [, ratingsCount]] = ratings;
+// console.log(rating, ratingsCount);
+
+// //1.4
+// const ratingStars = [63405, 1808];
+// const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+
+//////////////////////////////////////////////////////////////////////
+///////////////////////// The Spread Operator (...) //////////////////
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+//if we don't right ... it will be [1,2,[7,8,9]]
+//but if it has ... it will be [1,2,7,8,9]
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+//it will be 1,2,7,8,9 without []
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//Copy array - shallow copy
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 arrays
+const memu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jona';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+console.log(...str); //like: console.log('J','o','n','a');
+console.log('j', 'o');
+
+//Not work
+//Multiple values separated by a comma are usually only expected when we pass arguments into a function, or when we build a new array.
+// console.log(`${...str} Schmedtmann`);
+
+//Real-world example
+const ingredients = [
+  prompt("Let's make pasta! Ingredient 1?"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3'),
 ];
-//1.1 destructure the books array into two variables called firstBook and secondBook.
-const [firstBook, secondBook] = books;
 
-//1.2 Destructure the books array into a variable called thirdBook. You must skip the first two books.
-const [, , thirdBook] = books;
+console.log(ingredients);
 
-//1.3 setting the rating and ratingsCount variable with number in ratings.
-const ratings = [
-  ['rating', 4.19],
-  ['ratingsCount', 144584],
-];
-const [[, rating], [, ratingsCount]] = ratings;
-console.log(rating, ratingsCount);
+//Both of these are the same.
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
 
-//1.4
-const ratingStars = [63405, 1808];
-const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+//Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+//We did make a copy of the original restaurant.
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
